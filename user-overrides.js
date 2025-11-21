a819de9c0746be851ad4abff3ec334e45d5db530
Your Name <you@example.com>
2025-11-21 10:52:42 -0600

    clear history and downloads on firefox quit

--- user-overrides.js
+++ user-overrides.js
@@ -2,3 +2,4 @@
 // user_pref("preference.name", value);
 user_pref("browser.toolbars.bookmarks.visibility", "never"); // hide bookmarks on new tab
 user_pref("browser.urlbar.suggest.topsites", false); // disable url bar shortcuts
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", true);
